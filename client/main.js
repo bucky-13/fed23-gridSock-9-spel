0353301eaf34139cecbc80738e042067499c292b
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-02-27 13:12:59 +0100

    added socket-client, removed unused vite files

--- client/main.js
+++ client/main.js
@@ -1,24 +1,24 @@
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
+import { io } from 'socket.io-client';
+const socket = io('http://localhost:3000');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Human Person!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+let sendMessage = document.querySelector('#sendMessage');
+let sendBtn = document.querySelector('#sendBtn');
+let chatList = document.querySelector('#chatList');
 
-setupCounter(document.querySelector('#counter'));
+socket.on('chat', (argh) => {
+  console.log('SOCKET!', argh);
+  updateChat(argh);
+});
+
+let user = 'tim';
+
+function updateChat(chat) {
+  let li = document.createElement('li');
+  li.innerText = `${chat.user}: ${chat.message}`;
+  chatList.appendChild(li);
+}
+
+sendBtn.addEventListener('click', () => {
+  socket.emit('chat', { message: sendMessage.value, user: user });
+  console.log('sent chat', sendMessage.value);
+});
