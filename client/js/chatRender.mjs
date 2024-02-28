ac69941511fb82162a35ee1ae42838ec66225f67
MustafaaMehdi <anfsmus@gmail.com>
2024-02-29 00:28:08 +0100

    :wrench: chore(client) refactor modules

---
+++ client/js/chatRender.mjs
@@ -0,0 +1,35 @@
+import socket from "../lib/socket.mjs"
+import createElement from "../lib/createElement.mjs"
+import updateChat from "./updateChat.mjs"
+
+export default function chatRender(mainContainer) {
+    let existingChat = document.getElementById('chatbox')
+    if (existingChat) {
+        existingChat.remove()
+    }
+    let user = localStorage.getItem('username');
+    console.log(user);
+    const chatContainer = createElement('section', 'chatContainer', 'chatContainer');
+    mainContainer.appendChild(chatContainer);
+
+    const chatBox = createElement('ul', 'chatBox', 'chatBox');
+    const sendMessageLabel = createElement('label', 'sendMessageLabel', 'sendMessageLabel', 'Message: ');
+    const sendMessageInput = createElement('input', 'sendMessageInput', 'sendMessageInput');
+    sendMessageInput.placeholder = 'Write something';
+    sendMessageLabel.appendChild(sendMessageInput);
+
+    const sendMessageBtn = createElement('button', 'sendMessageBtn', 'sendMessageBtn', 'Send');
+
+    sendMessageBtn.addEventListener('click', () => {
+        console.log('1');
+        socket.emit('chat', { user: user, message: sendMessageInput.value });
+    });
+
+    socket.on('chat', (arg) => {
+        console.log('2');
+        console.log('socket', arg);
+        updateChat(arg, chatBox);
+    });
+
+    chatContainer.append(chatBox, sendMessageLabel, sendMessageBtn);
+}
