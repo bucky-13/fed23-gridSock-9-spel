5f00a2c0b8bb81c67d28bf31263abb03da556226
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-02-29 15:54:42 +0100

    set new chat messages to appear on top

--- client/js/updateChat.mjs
+++ client/js/updateChat.mjs
@@ -1,6 +1,7 @@
 import createElement from "../lib/createElement.mjs";
 
 export default function updateChat(chat, chatBox) {
+    let topChat = document.querySelector('.newMessage');
     let newMessage = createElement('li', 'newMessage', 'newMessage', `${chat.user}: ${chat.message}`)
-    chatBox.appendChild(newMessage)
+    chatBox.insertBefore(newMessage, topChat)
 }
