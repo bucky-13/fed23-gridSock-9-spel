94c1dcafd4c8065a82af77d5874d58aa8aed9e55
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-03-05 16:53:39 +0100

    added a functional database connection to server

--- server/app.js
+++ server/app.js
@@ -2,11 +2,24 @@ const { read } = require('fs');
 
 const app = require('express')();
 const server = require('http').createServer(app);
+const cors = require('cors');
 const io = require('socket.io')(server, {
     cors: {
         origin: '*',
         methods: ['GET', 'POST']
     }
+});
+
+// SETUP FOR DATABASE CONFIGS
+require('dotenv').config();
+const mysql = require('mysql2')
+
+app.locals.con = mysql.createConnection({
+  host: process.env.SQL_IP,
+  port: process.env.SQL_PORT,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DATABASE,
 });
 
 // IMPORTING MODULES 
@@ -18,10 +31,26 @@ const handleChat = require('./chat/chat')
 const handlePlayerReady = require('./game/playerReady')
 const { users, usersReady } = require('./lib/serverDatabase.js');
 
+app.use(cors());
 
-// probably not needed
+// gets a response from digital ocean with a test database
 app.get('/', (req,res) => {
-    res.send("<h1>Socket at server at DigitalOcean</h1>")
+    // res.send("<h1>Socket at server at DigitalOcean</h1>")
+     req.app.locals.con.connect(function (err) {
+    if (err) {
+      console.log(err);
+    }
+
+    // GET all notes from a specific user
+    let sql = `SELECT * FROM test`;
+
+    req.app.locals.con.query(sql, function (err, result) {
+      if (err) {
+        console.log(err);
+      }
+      res.json(result);
+    });
+  });
 })
 
 // CONNECTION FUNCTION, CALL IMPORTED FUNCTIONS HERE. PASS IN ANY ARRAYS OR OBJECTS THEY NEED TO HANDLE
