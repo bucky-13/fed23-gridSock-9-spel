fc0bf70702613dad870ad5f1e365d23fe9f3c8b6
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-03-06 10:28:20 +0100

    added get/post routes for test3 on database

--- server/app.js
+++ server/app.js
@@ -41,17 +41,100 @@ app.get('/', (req,res) => {
       console.log(err);
     }
 
-    // GET all notes from a specific user
-    let sql = `SELECT * FROM test`;
+    // GET all in test collection
+    let sql = `SELECT * FROM test3`;
 
     req.app.locals.con.query(sql, function (err, result) {
       if (err) {
         console.log(err);
       }
-      res.json(result);
+
+      // Converts to normal arrays, use on colors and players
+        let players = result[0].players.split(',')
+        let colors = result[0].colors.split(',')
+
+        // First conversion of 2 dimensional array into one long array with strings
+        let grid1dString = result[0].grid.split(',')
+        // Changes the strings in the 2d array into numbers (int)
+        let grid1dInt = grid1dString.map(function (str) {
+            return parseInt(str);
+        })
+
+        // This is the final version of the 2 dimensional array
+        let grid = [];
+        // This is each row that gets inserted
+        let gridColumns = [];
+
+        // Loops over the entire grid array to turn it into a 2d array again
+        for (let i = 0; i < grid1dInt.length; i++) {
+            
+            gridColumns.push(grid1dInt[i])
+
+            // When each row have X amount of entries, it gets pushed to grid and empties so a new row can be created
+            if (gridColumns.length >= result[0].gridColumns) {
+                grid.push(gridColumns);
+                gridColumns = [];
+            }
+      }
+
+      let currentGameboard = {
+        id: result[0].testId,
+        gridColumns: result[0].gridColumns,
+        players: players,
+        colors: colors,
+        grid: grid
+      };
+ 
+
+      
+
+      res.json(currentGameboard);
     });
   });
 })
+
+app.post('/', function (req, res, next) {
+  req.app.locals.con.connect(function (err) {
+    if (err) {
+      console.log(err);
+    }
+      // let sqlQuery = `INSERT INTO test2 (array) VALUES ("${gameboard1.colors}")`;
+
+      let sqlQuery = `INSERT INTO test3 (gridColumns, players, colors, grid) VALUES ("${gameboard1.gridColumns}", "${gameboard1.players}", "${gameboard1.colors}", "${gameboard1.grid}")`;
+
+    req.app.locals.con.query(sqlQuery, function (err, result) {
+      if (err) {
+        console.log(err);
+        }
+         res.json(result);
+    });
+  });
+});
+
+let gameboard1 = {
+    id: 1,
+    gridColumns: 15,
+    players: [1,2,3,4],
+    colors: ['green', 'blue', 'white', 'red'],
+    grid: [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
+    ]
+}
 
 // CONNECTION FUNCTION, CALL IMPORTED FUNCTIONS HERE. PASS IN ANY ARRAYS OR OBJECTS THEY NEED TO HANDLE
 const onConnection = (socket) => {
@@ -60,9 +143,10 @@ handleLogout(io, socket, users, usersReady);
 handleChat(io, socket, users, usersReady);
 handleActivity(io, socket, users, usersReady);
 handleDisconnect(io, socket, users, usersReady);
-handlePlayerReady(io, socket, users, usersReady);
-    
+handlePlayerReady(io, socket, users, usersReady);  
 }
+
+
 
 // INITIATING THE CONNECTION FUNCTION
 io.on('connection', onConnection);
