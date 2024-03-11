c10a91d1844796b571c44eb8a7d5c34644041554
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-03-11 09:33:08 +0100

    added display one color for each player when selecting new game

--- client/js/game/createNewGameSocket.mjs
+++ client/js/game/createNewGameSocket.mjs
@@ -1,4 +1,5 @@
 import socket from "../../lib/socket.mjs";
+import renderAssignedColor from "./renderAssignedColor.mjs";
 
 export default function createNewGameSocket() { 
     
@@ -7,6 +8,7 @@ export default function createNewGameSocket() {
         console.log(arg);
         //Stores color player is using in local storage
         localStorage.setItem('gameboardColor', arg);
+        renderAssignedColor();
     })
     
 }
