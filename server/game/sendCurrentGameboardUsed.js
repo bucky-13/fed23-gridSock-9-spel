d8c23a3adb0f05cfffc7b36a6bafc0ffa6f7e9c5
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-03-11 16:24:08 +0100

    added count reset to sendCurrentGameboardsUsed

--- server/game/sendCurrentGameboardUsed.js
+++ server/game/sendCurrentGameboardUsed.js
@@ -10,9 +10,12 @@ module.exports = (io, socket, users, rooms, currentGameboardsUsed) => {
 
 
         // CHANGE 2 INTO 4 WHEN DEPLOYING ------------IMPORTANT!
+        // This if statement makes sure that the emit to frontend from the timer only happens after it's recieved a call from all 4 players. (2 currently so we only have to test with 2 players)
         if (playersReady[roomId] >= 2) {
             let gameboardOfArgh = currentGameboardsUsed[roomId]
             io.emit('recieveCurrentGameboardUsed', gameboardOfArgh)
+            playersReady[roomId] = 0
+            // console.log(playersReady[roomId]);
         }
         
 
