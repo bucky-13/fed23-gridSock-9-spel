8e88bf1811cd67ef344a8a049c43d45c6e202fe3
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-03-12 10:24:01 +0100

    update gameview

--- client/js/game/renderCurrentGameboardUsed.mjs
+++ client/js/game/renderCurrentGameboardUsed.mjs
@@ -4,10 +4,29 @@ import socket from "../../lib/socket.mjs";
 let gameSection = document.getElementById('gameSection');
 
 
-export default function renderCurrentGameboardUsed(arg) { 
+export default function renderCurrentGameboardUsed(currentGame) { 
+    let gameboardContainer = document.createElement('div');
+    gameboardContainer.id = 'gameboardContainer';
+    gameboardContainer.classList.add('gameboardContainer');
+    gameboardContainer.style.backgroundColor = '#ffffff';
+
+    gameSection.appendChild(gameboardContainer);
+    for (let i = 0; i < currentGame.length; i++) {
+        for (let j = 0; j < currentGame[i].length; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell'); 
+            cell.id = `cell-(${i},${j})`;
+            cell.style.backgroundColor = "#f5f5dc";
+            cell.textContent = currentGame[i][j];
+
+        
+            gameboardContainer.appendChild(cell);
+        }
+    }
+
     
     let test = createElement('p')
-    test.textContent = `I appear after 5 seconds because I am 5 seconds big :D My name is ${arg.description}`
-    gameSection.append(test)
-    console.log(arg);
+    test.textContent = `I appear after 5 seconds because I am 5 seconds big :D My name is ${currentGame.description}`
+    gameSection.append(test, gameboardContainer);
+    console.log(currentGame);
 }
