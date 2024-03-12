bfe5b8d68a6b11d4c3bf288c8cc83c5b304327cc
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-03-12 13:32:03 +0100

    create active gameboard, store it in server, send to frontend

--- client/js/game/renderCurrentGameboardUsed.mjs
+++ client/js/game/renderCurrentGameboardUsed.mjs
@@ -5,9 +5,19 @@ let gameSection = document.getElementById('gameSection');
 
 
 export default function renderCurrentGameboardUsed(arg) { 
+    let roomId = localStorage.getItem('roomId')
     
     let test = createElement('p')
     test.textContent = `I appear after 5 seconds because I am 5 seconds big :D My name is ${arg.description}`
     gameSection.append(test)
     console.log(arg);
+
+    setTimeout(function () {
+        socket.emit('generateActiveGame', roomId)
+        socket.on('recieveActiveGame', (arg) => { 
+            console.log('recieveActiveGame');
+            console.log(arg);
+        })
+    }, 1000);
+
 }
