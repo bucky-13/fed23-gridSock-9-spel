49d8b4b68a0c6c94b8962b663cc2a9316ac2e8cb
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-03-12 16:50:08 +0100

    fixed updating backend activeGame grid functionality

--- server/game/updateActiveGameboardServer.js
+++ server/game/updateActiveGameboardServer.js
@@ -2,9 +2,18 @@
 module.exports = (io, socket, users, rooms, currentGameboardsUsed, activeGames) => {
     socket.on('updateActiveGameboardServer', (roomId, x, y, color) => { 
         console.log(activeGames[roomId].grid[x][y]);
-                  
-            let clientInfo = [x, y, color]
+
+        // Finds the index of the color sent from client
+        let colorIndex = activeGames[roomId].colors
+        let index = colorIndex.findIndex((col) => col === color)
+        
+        // updates the activeGamesGrid with the changed cell
+        activeGames[roomId].grid[x][y] = index
+
+        // Array that gets sent back to client, not sure if last one is needed, right now only used for testing     
+        let clientInfo = [x, y, color, activeGames[roomId]]
  
-            io.emit('updateActiveGameboardClient', clientInfo)
+        io.emit('updateActiveGameboardClient', clientInfo)
         })
- }
+}
+ 
