eaab022a6bfc2925d90a82c2058092229ab8614e
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-03-13 13:14:33 +0100

    initial function to calculate scores

---
+++ server/game/calculateScore.js
@@ -0,0 +1,28 @@
+const { activeGames } = require("../lib/serverDatabase");
+
+function calculateScore(activeGame, currentGame, roomId) {
+    let correctCount = 0;
+    const totalCells = activeGame.length * activeGame[0].length;
+
+    for (let i = 0; i < activeGame.length; i++) {
+        for (let j = 0; j < activeGame[i].length; j++) {
+            if (activeGame[i][j] === currentGame[i][j]) {
+                correctCount++;
+            }
+        }
+    }
+
+    const score = (correctCount / totalCells) * 100;
+    return {roomId, score };
+}
+
+function handleGameFinished(io, socket, users, rooms, currentGameboardsUsed, activeGames) {
+    socket.on('gameFinished', (roomId) => {
+        const activeGame = activeGames[roomId];
+        const currentGame = currentGameboardsUsed[roomId];
+        const result = calculateScore(activeGame.grid, currentGame, roomId);
+        io.to(roomId).emit('gameResult', result);
+    })
+}
+
+module.exports = { calculateScore, handleGameFinished };
